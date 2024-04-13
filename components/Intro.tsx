96192a3c992d3ccdc4787f08d03e2f15a8cc7b15
Chandra Prakash <chandraprakash1104@gmail.com>
2024-04-13 23:09:47 +0530

    intro buttons fixed

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -17,7 +17,7 @@ const Intro = () => {
     <div
       ref={ref}
       id='home'
-      className='my-28 max-w-[65rem] text-center sm:mb-0 scroll-mt-[100rem] text-slate-300'
+      className='mt-52 mb-28 max-w-[65rem] text-center sm:mb-0 scroll-mt-[100rem] text-slate-300'
     >
       <div className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-3xl'>
         <span className='font-bold'>Hello, I&apos;m Chandra Prakash.</span>{' '}
@@ -46,22 +46,23 @@ const Intro = () => {
           Download CV{' '}
           <HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
         </Link>
+        <div className='flex space-x-5'>
+          <Link
+            className='bg-white p-4 text-gray-700 hover:text-[#0077B5] flex items-center gap-2 rounded-xl focus:scale-[1.10] hover:scale-[1.10] active:scale-105 transition cursor-pointer borderBlack '
+            href='https://www.linkedin.com/in/chandraprakash1104/'
+            target='_blank'
+          >
+            <BsLinkedin />
+          </Link>
 
-        <Link
-          className='bg-white p-4 text-gray-700 hover:text-[#0077B5] flex items-center gap-2 rounded-xl focus:scale-[1.10] hover:scale-[1.10] active:scale-105 transition cursor-pointer borderBlack '
-          href='https://www.linkedin.com/in/chandraprakash1104/'
-          target='_blank'
-        >
-          <BsLinkedin />
-        </Link>
-
-        <Link
-          className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-xl focus:scale-[1.10] hover:scale-[1.10] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack '
-          href='https://github.com/ChandraPrakash1104'
-          target='_blank'
-        >
-          <FaGithubSquare />
-        </Link>
+          <Link
+            className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-xl focus:scale-[1.10] hover:scale-[1.10] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack '
+            href='https://github.com/ChandraPrakash1104'
+            target='_blank'
+          >
+            <FaGithubSquare />
+          </Link>
+        </div>
       </div>
     </div>
   );
